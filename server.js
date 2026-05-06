01027780ecdb394ea3163e7d835de8b8afbddd92
amax_theskywithsunshine <aongsakenmimi@gmail.com>
2026-05-06 15:17:03 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
@@ -6,6 +7,14 @@ const PORT = process.env.PORT || 3000;
 
 // Serve static files from the current directory
 app.use(express.static(__dirname));
+
+// Expose env vars to the frontend (never expose secrets you don't want public)
+app.get('/config', (req, res) => {
+  res.json({
+    YOUTUBE_HANDLE:  process.env.YOUTUBE_HANDLE  || '',
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY || '',
+  });
+});
 
 // For any route, serve the index.html file (useful if you ever add client-side routing)
 app.get('*', (req, res) => {
